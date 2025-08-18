5fafd8c4d7adc872347f072d402134cc715e543a
Rafail Miliopoulos <rafail.miliopoulos@nl.abnamro.com>
2025-08-18 23:48:20 +0200

    removing deprecated code

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,6 @@ const router = createRouter({
                     component: () => import('@/views/Dashboard.vue')
                 },
                 {
-                    path: '/financial-dashboard',
-                    name: 'financial-dashboard',
-                    component: () => import('@/views/ExpensesDashboard.vue')
-                },
-                {
                     path: '/demo-functionalities',
                     name: 'demo-functionalities',
                     component: () => import('@/views/DemoFunctionalities.vue')
