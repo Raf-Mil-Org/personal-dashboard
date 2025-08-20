18b6a7b5203c86b772d7be05fc0b922bd980a7ae
Rafail Miliopoulos <rafail.miliopoulos@nl.abnamro.com>
2025-08-20 22:51:21 +0200

    removing unued import function

--- src/composables/useTransactionEngine.js
+++ src/composables/useTransactionEngine.js
@@ -30,7 +30,7 @@ const STORAGE_KEYS = {
 
 export function useTransactionEngine() {
     // Initialize learning system
-    const { initializeLearning, learnFromAssignment, applyLearnedRules, clearLearnedData, exportLearnedRules, importLearnedRules, totalRules, totalAssignments } = useTransactionLearning();
+    const { initializeLearning, applyLearnedRules, clearLearnedData, exportLearnedRules, importLearnedRules, totalRules, totalAssignments } = useTransactionLearning();
 
     // Initialize tag mapping system
     const { getTagMapping: getOriginalTagMapping } = useMultiFormatParser();
